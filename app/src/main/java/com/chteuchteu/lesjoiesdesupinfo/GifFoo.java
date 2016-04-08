19181c6f5fe3cea392bec9f0db3df25c73f7eaa0
Quentin Stoeckel <chteuchteu@gmail.com>
2016-04-08 22:35:40 +0200

    Update app settings, strings & theme

--- app/src/main/java/com/chteuchteu/lesjoiesdesupinfo/GifFoo.java
+++ app/src/main/java/com/chteuchteu/lesjoiesdesupinfo/GifFoo.java
@@ -5,21 +5,35 @@ import android.content.Context;
 import com.chteuchteu.gifapplicationlibrary.i.IDataSourceParser;
 import com.chteuchteu.gifapplicationlibrary.obj.Gif;
 import com.chteuchteu.gifapplicationlibrary.obj.GifApplicationBundle;
-import com.chteuchteu.lesjoiesdesupinfo.hlpr.RSSReader;
+import com.chteuchteu.lesjoiesdesupinfo.hlpr.APIConsumer;
 import com.chteuchteu.lesjoiesdesupinfo.serv.NotificationService;
 import com.chteuchteu.lesjoiesdesupinfo.ui.Activity_Main;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class GifFoo {
+	public static String[] getSupportedTypes() {
+		return new String[]{"gif"};
+	}
+
+	/**
+	 * Returns true if this file type is supported
+	 * @param type String
+	 * @return boolean
+	 */
+	public static boolean supports(String type) {
+		return Arrays.asList(GifFoo.getSupportedTypes()).contains(type);
+	}
+
 	public static GifApplicationBundle getApplicationBundle(Context context) {
 		return new GifApplicationBundle(
 				context.getString(R.string.app_name),
-				"http://joies-de-supinfo.s-quent.in/feed",
+				"http://www.joies-de-supinfo.fr/api/gif/list?count=75",
 				new IDataSourceParser() {
 					@Override
 					public List<Gif> parseDataSource(String dataSourceUrl) {
-						return RSSReader.parse(dataSourceUrl, null);
+						return APIConsumer.parse(dataSourceUrl, null);
 					}
 				},
 				"lesJoiesDeSupinfo",
